ce8abf7e334fef7c62de0dfc20da0ba463476922
alexpac27 <alexpac27@gmail.com>
2020-07-19 01:36:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+const driver = {body: 'squish'};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = {}
+    newDriver[key] = value
+    const newNew = Object.assign({},driver, newDriver)
+    return newNew;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+    driver[key] = value;
+    return driver
+};
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = {...driver};
+    delete newDriver[key]
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
+
+
